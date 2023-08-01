f038a7071ec5d206787bd1c5301ddba93058e8c3
Artsiom Lipchyk <tommerg09@gmail.com>
2023-08-01 18:32:03 +0300

    code refactoring

--- src/components/pages/cart-page.js
+++ src/components/pages/cart-page.js
@@ -8,18 +8,6 @@ const CartPage = () => {
     <div className='cart-page-container'>
       <ShoppingCartList />
       <ShoppingCartForm />
-      <div class="select">
-        <select id="standard-select">
-          <option value="Option 1">Option 1</option>
-          <option value="Option 2">Option 2</option>
-          <option value="Option 3">Option 3</option>
-          <option value="Option 4">Option 4</option>
-          <option value="Option 5">Option 5</option>
-          <option value="Option length">
-            Option that has too long of a value to fit
-          </option>
-        </select>
-      </div>
     </div>
   );
 };

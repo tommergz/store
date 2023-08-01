3f3c2aa56ae31cc997078610d4ce8582d244925b
Artsiom Lipchyk <tommerg09@gmail.com>
2023-08-01 15:13:42 +0300

    change data

--- src/data/data.js
+++ src/data/data.js
@@ -1,50 +1,50 @@
 export const data = [
   {
-    id: 1,
-    title: 'Кровать TARTAN',
-    description: 'Основание из полированной нержавеющей стали, придает оригинальный парящий эффект.',
-    price: 120000,
-    coverImage: 'tartan',
-    date: "Mar 16 2023 10:00:00 AM"
+    "id": 1,
+    "title": "Кровать TARTAN",
+    "description": "Основание из полированной нержавеющей стали, придает оригинальный парящий эффект.",
+    "price": 120000,
+    "coverImage": "tartan",
+    "date": "Mar 16 2023 10:00:00 AM"
   },
   {
-    id: 2,
-    title: 'Кресло VILORA',
-    description: 'Мягкое и уютное, аккуратное и стильное. Упругие подушки сиденья и приятная на ощупь ткань. ',
-    price: 21000,
-    coverImage: 'vilora',
-    date: "Mar 15 2023 10:00:00 AM"
+    "id": 2,
+    "title": "Кресло VILORA",
+    "description": "Мягкое и уютное, аккуратное и стильное. Упругие подушки сиденья и приятная на ощупь ткань.",
+    "price": 21000,
+    "coverImage": "vilora",
+    "date": "Mar 15 2023 10:00:00 AM"
   },
   {
-    id: 3,
-    title: 'Стол MENU',
-    description: 'Европейский дуб - отличается особой прочностью и стабильностью.',
-    price: 34000,
-    coverImage: 'menu',
-    date: "Mar 14 2023 10:00:00 AM"
+    "id": 3,
+    "title": "Стол MENU",
+    "description": "Европейский дуб - отличается особой прочностью и стабильностью.",
+    "price": 34000,
+    "coverImage": "menu",
+    "date": "Mar 14 2023 10:00:00 AM"
   },
   {
-    id: 4,
-    title: 'Диван ASKESTA',
-    description: 'Благодаря защелкивающемуся механизму диван легко раскладывается в комфортную кровать',
-    price: 68000,
-    coverImage: 'askesta',
-    date: "Mar 17 2023 10:00:00 AM"
+    "id": 4,
+    "title": 'Диван ASKESTA',
+    "description": "Благодаря защелкивающемуся механизму диван легко раскладывается в комфортную кровать",
+    "price": 68000,
+    "coverImage": "askesta",
+    "date": "Mar 17 2023 10:00:00 AM"
   },
   {
-    id: 5,
-    title: 'Кресло LUNAR',
-    description: 'Прекрасно переносит солнечные лучи, перепады влажности и любые осадки',
-    price: 40000,
-    coverImage: 'lunar',
-    date: "Mar 18 2023 10:00:00 AM"
+    "id": 5,
+    "title": "Кресло LUNAR",
+    "description": "Прекрасно переносит солнечные лучи, перепады влажности и любые осадки",
+    "price": 40000,
+    "coverImage": "lunar",
+    "date": "Mar 18 2023 10:00:00 AM"
   },
   {
-    id: 6,
-    title: 'Шкаф NASTAN',
-    description: 'Мебель может быть оснащена разнообразными системами подсветки.',
-    price: 80000,
-    coverImage: 'nastan',
-    date: "Mar 19 2023 10:00:00 AM"
+    "id": 6,
+    "title": "Шкаф NASTAN",
+    "description": "Мебель может быть оснащена разнообразными системами подсветки.",
+    "price": 80000,
+    "coverImage": "nastan",
+    "date": "Mar 19 2023 10:00:00 AM"
   }
 ];
